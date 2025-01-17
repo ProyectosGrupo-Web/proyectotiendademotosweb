42d13cbe898ab36a92acb5658f7106f0a000cdfd
JhonnyARM <angelesjhonny5@gmail.com>
2025-01-16 23:26:49 -0500

    Inicio

--- webmotos/obj/Debug/net8.0/webmotos.AssemblyInfo.cs
+++ webmotos/obj/Debug/net8.0/webmotos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webmotos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22e0116a8b332646eb460f541a839e463ef9a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("webmotos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webmotos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
